d7ccc93b117936a2f7dc1660c5a1a69f06b69c2d
Johannes <johannesr@viken.no>
2023-02-22 02:58:33 +0100

    cleaned up section base class even more

--- editor/section/section.py
+++ editor/section/section.py
@@ -9,21 +9,7 @@ class Section:
         self.surf = pygame.surface.Surface(dimensions)
         self.color = color
         self.surf.fill(self.color)
-        self.sprites = []
-        self.load_sprites()
-
-    def load_sprites(self, path="assets/imgs"):
-        try:
-            file_names = os.listdir(path)
-            for file_name in file_names:
-                file_path = os.path.join(path, file_name)
-                self.sprites.append(pygame.image.load(file_path))
-        except FileNotFoundError:
-            print(f"asset path invalid: '{path}'")
 
     def draw(self):
         self.surf.fill(self.color)
-        for i, sprite in enumerate(self.sprites):
-            self.surf.blit(sprite, (0,i*TILE_SIZE))
-
 
