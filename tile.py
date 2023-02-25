373f11ca57f87678063093bb9e2afc0d31dca784
Johannes <johannes>
2023-02-25 01:22:10 +0100

    major refactor begun

--- tile.py
+++
@@ -1,6 +0,0 @@
-import pyglet
-
-class Tile:
-    def __init__(self, x, y, width, height, color, batch) -> None:
-        self.rect = pyglet.shapes.Rectangle(x, y, width, height, color, batch)
-        self.relative_pos = (x, y)
